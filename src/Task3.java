f3fb4814bcfbaf73a85192635112d54cc1f9526b
Glebova <e.tulzakova@mail.ru>
2023-01-15 18:08:33 +0400

    ДЗ выполнено

---
+++ src/Task3.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task3 {
+    private final Map<String, Integer> map = new HashMap<>();
+
+    public void addToMap(String key, int value){
+        if (!map.containsKey(key)){
+            map.put(key, value);
+        }else{
+            int valueFromMap = map.get(key);
+            if (valueFromMap == value) {
+                throw new IllegalArgumentException();
+            }else {
+                map.put(key,valueFromMap + value);
+                }
+            }
+        }
+
+    @Override
+    public String toString() {
+        return "Task3, map" + map;
+    }
+}
+
